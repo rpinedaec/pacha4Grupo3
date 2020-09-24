980609e185b76001a59becac4b8654b2fc85ec9e
JWilian <wilianders.uni@gmail.com>
2020-09-23 22:51:58 -0500

    commit wilian

--- ecommapp/serializers.py
+++ ecommapp/serializers.py
@@ -1,8 +1,62 @@
 from rest_framework import serializers
-from .models import detalle_pedido
 
-class DetalleSerializer(serializers.Serializer):
+from ecommapp.models import *
+
+class ProductoSerializer(serializers.ModelSerializer):
+
+    def retrieve(self, data):
+        if not data:
+            raise serializers.ValidationError({"data": "No existe Curso", "error": True})
+    class Meta:
+        model = producto
+        fields = '__all__'
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    
+    def retrieve(self, data):
+        if not data:
+            raise serializers.ValidationError({"data": "No existen categorias", "error": True})
+    class Meta:
+        model = categoria
+        fields = '__all__'
+
+class CuponSerializer(serializers.ModelSerializer):
+
+    def retrieve(self, data):
+        if not data:
+            raise serializers.ValidationError({"data": "No existe Cupon", "error": True})
 
     class Meta:
+        model = cupon
+        fields = '__all__'
+
+class ClienteSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = cliente
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    
+    def retrieve(self, data):
+        if not data:
+            raise serializers.ValidationError({"data": "No existen Pedidos", "error": True})
+    class Meta:
+        model = pedido
+        fields = '__all__'
+
+class detallePedidoSerializer(serializers.ModelSerializer):
+    
+    def validate(self, data):
+        if not data["pedido"]:
+            raise serializers.ValidationError({"data": "Se requiere # de Pedido para detalle", "error": True})
+        return data
+    class Meta:
         model = detalle_pedido
+        fields = '__all__'
+
+class estadoPedidoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = estado_pedido
         fields = '__all__'
--- ecommapp/views.py
+++ ecommapp/views.py
@@ -1,11 +1,130 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from .models import detalle_pedido
-from .serializers import DetalleSerializer
+#from django.shortcuts import render
 from rest_framework import viewsets
+from ecommapp.serializers import *
+from ecommapp.models import *
 
-class DetalleViewSet(viewsets.ModelViewSet):
-    queryset = detalle_pedido.objects.all()
-    serializer_class = DetalleSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    
+    def get_queryset(self):
+        queryset = producto.objects.all()
+        prodNom = self.request.query_params.get('nombre',None)
+        prodId = self.request.query_params.get('id',None)
+        if prodNom is not None:
+            queryset = queryset.filter(nombre=prodNom)
+        if prodId is not None:
+            queryset = queryset.filter(id=prodId)
+        if not queryset:
+            ProductoSerializer.retrieve(self, queryset)
+        return queryset
+
+    serializer_class = ProductoSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    
+    def get_queryset(self):
+        #queryset = categoria.objects.all()
+        queryset = categoria.objects.filter()
+        if not queryset:
+            CategoriaSerializer.retrieve(self, queryset)
+        return queryset
+    
+    serializer_class = CategoriaSerializer
+
+class CuponViewSet(viewsets.ModelViewSet):
+    
+    def get_queryset(self):
+        queryset = cupon.objects.filter()
+        serializer = CuponSerializer(queryset, many=True)
+        cupCod = self.request.query_params.get('codigo',None)
+        if cupCod is not None:
+            queryset = queryset.filter(codigo=cupCod)
+        if not queryset:
+            CuponSerializer.retrieve(self, queryset)
+        return queryset
+    serializer_class = CuponSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    
+    queryset = cliente.objects.filter()
+    serializer_class = ClienteSerializer
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    
+    def get_queryset(self):
+        queryset = pedido.objects.all()
+        clieId = self.request.query_params.get('cliente',None)
+        if clieId is not None:
+            queryset = queryset.filter(cliente=clieId)
+        if not queryset:
+            PedidoSerializer.retrieve(self, queryset)
+        return queryset
+    serializer_class = PedidoSerializer
+
+class detallePedidoViewSet(viewsets.ModelViewSet):
+    
+    def get_queryset(self):
+        queryset = detalle_pedido.objects.all()
+        pedId = self.request.query_params.get('pedido',None)
+        if pedId is not None:
+            queryset = queryset.filter(pedido=pedId)
+        return queryset
+    def post(self, request):
+        pedId = self.request.query_params.get('pedido',None)
+        if pedId is not None:
+            queryset = pedido.objects.all()
+            queryset = queryset.filter(id=pedId)
+        if not queryset:
+            detallePedidoSerializer.validate(self, queryset)
+        detallePedidoSerializer.save()
+        return JsonResponse({"data": list(queryset), "error": False})
+    serializer_class = detallePedidoSerializer
+
+class estadoPedidoViewSet(viewsets.ModelViewSet):
+    queryset = estado_pedido.objects.all()
+    serializer_class = estadoPedidoSerializer
+
+@csrf_exempt
+def getPedido(request):
+    queryset = pedido.objects.all().values()
+    if request.method == 'POST':
+        try:
+            jsonData = json.loads(request.body)
+            idPedido = int(jsonData["idPedido"])
+            queryset = queryset.filter(id=idPedido)
+
+            if queryset:
+                querysetDet = detalle_pedido.objects.all().values()
+                querysetDet = querysetDet.filter(pedido=idPedido)
+            else:
+                return JsonResponse({"data": "No existe Pedido", "error": True})
+        except Exception as e:
+            return JsonResponse({"data": e, "error": True})
+
+    #return JsonResponse({"mensaje": "ok"})
+    return JsonResponse({"pedido": list(queryset), "detalle": list(querysetDet), "error": False})
+
+@csrf_exempt
+def loginCliente(request):
+    queryset = cliente.objects.all().values()
+    if request.method == 'POST':
+        try:
+            jsonData = json.loads(request.body)
+            cliUname = str(jsonData["username"])
+            cliPass = str(jsonData["password"])
+            if cliUname is not None:
+                queryset = queryset.filter(username=cliUname, password=cliPass)
+                if not queryset:
+                    return JsonResponse({"data": "Credenciales incorrectas", "error": True})
+        except Exception as e:
+            return JsonResponse({"data": e, "error": True})
+
+    return JsonResponse({"cliente": list(queryset), "error": False})
